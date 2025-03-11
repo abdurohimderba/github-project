f38717acb338923192de83ab57a3fb7a3bee0c1a
abdurohimderba <abdurohimderba@gmail.com>
2025-03-11 15:51:17 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,10 @@
+<?php
+// Get the current timestamp
+$timestamp = time();
+
+// Generate a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// Print the result
+echo $timestamp . ' - ' . $randomNumber;
+?>
