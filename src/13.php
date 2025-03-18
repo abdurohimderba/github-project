e70e220195772bdf600a1ccb609d4ce8941a2418
abdurohimderba <abdurohimderba@gmail.com>
2025-03-18 21:59:18 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,10 @@
+  <?php
+$num = rand(0,10);
+if ($num == 0) {
+    echo "The number is zero";
+} else if ($num <= 5) {
+    echo "The number is less than or equal to five";
+} else {
+    echo "The number is greater than five";
+}
+?>
