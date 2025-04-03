f5817eec45789b4dab4b869a132db6da0b0a79a8
abdurohimderba <abdurohimderba@gmail.com>
2025-04-03 13:55:27 +0000

    Add 25.php

---
+++ src/25.php
@@ -0,0 +1,8 @@
+<?php
+// This is a sample PHP script.
+// It can be used to test or debug your code without running it in a live environment.
+// If you want to run this script and see its output, you need to make sure it's safe.
+
+// Example usage:
+// $output = perform_action();
+?>
